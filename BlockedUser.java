5089fd3269a394ebed3dd94b739a5f647bc6dc7f
Maninder Kaur <75804314+themaninderkaur@users.noreply.github.com>
2024-11-20 18:46:16 -0500

    Create BlockedUser.java

---
+++ BlockedUser.java
@@ -0,0 +1,11 @@
+public class BlockedUser  {
+    private long blockerId;
+    private long blockedId;
+
+    public BlockedUser (long blockerId, long blockedId) {
+        this.blockerId = blockerId;
+        this.blockedId = blockedId;
+    }
+
+    // Getters and Setters
+}
