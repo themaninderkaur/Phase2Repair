608f6ce41d9f4105b56467d0947eb052988acd76
Maninder Kaur <75804314+themaninderkaur@users.noreply.github.com>
2024-11-20 18:47:02 -0500

    Create Database.java

---
+++ Database.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Databases {
+    private ArrayList<User> users;
+    private ArrayList<Friend> friends;
+    private ArrayList<Message> messages;
+    private ArrayList<BlockedUser > blockedUsers;
+
+    public Databases() {
+        users = new ArrayList<>();
+        friends = new ArrayList<>();
+        messages = new ArrayList<>();
+        blockedUsers = new ArrayList<>();
+    }
+
+    // Methods to add entities and getters for the lists
+}
