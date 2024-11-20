0c95cd51c42e7d04c1a57042becdcb5cb5ab8231
Maninder Kaur <75804314+themaninderkaur@users.noreply.github.com>
2024-11-20 18:45:11 -0500

    Update User.java

--- User.java
+++ User.java
@@ -1,1 +1,23 @@
+import java.util.Date;
 
+public class User {
+    private long userId;
+    private String username;
+    private String password;
+    private String email;
+    private String profilePictureUrl;
+    private String bio;
+    private Date createdAt;
+
+    public User(long userId, String username, String password, String email, String profilePictureUrl, String bio) {
+        this.userId = userId;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.profilePictureUrl = profilePictureUrl;
+        this.bio = bio;
+        this.createdAt = new Date(); // Set to current date
+    }
+
+    // Getters and Setters
+}
