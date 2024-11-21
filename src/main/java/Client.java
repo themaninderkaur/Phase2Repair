b28ac5027e7b5441941f332a04eb4279a7d7efa7
adivirma <virmania@purdue.edu>
2024-11-21 18:26:00 -0500

    Update Client.java

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,3 +1,21 @@
-public class Client {
-    
+import java.io.*;
+import java.net.*;
+
+public class EchoClient {
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("localhost", 4343);  // Connect to the server
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        BufferedReader consoleInput = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Connected to Echo Server! Type your messages below:");
+
+        String userInput;
+        while ((userInput = consoleInput.readLine()) != null) {
+            out.println(userInput);  // Send message to the server
+            String serverResponse = in.readLine();  // Read the server's response
+            System.out.println(serverResponse);  // Print the server's response
+        }
+        socket.close();
+    }
 }
