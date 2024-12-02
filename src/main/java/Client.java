e7ab561f3d8af2248a2026f5a15f8e1d319b9935
Justin DeRosa <derosa0@purdue.edu>
2024-12-02 14:16:19 -0500

    Made the files thread safe, part of phase 2 corrections

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -6,17 +6,17 @@ import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) throws IOException {
-        Socket socket = new Socket("localhost", 4343);  // Connect to the server on port 4343
-        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));  // To read data from the server
-        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);  // To send data to the server
-        BufferedReader consoleInput = new BufferedReader(new InputStreamReader(System.in));  // To read user input
+        Socket socket = new Socket("localhost", 4343);
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        BufferedReader consoleInput = new BufferedReader(new InputStreamReader(System.in));
         boolean exit = false;
         Scanner s = new Scanner(System.in);
 
         try {
             System.out.println("Connected to the Social Media Server!");
 
-            while (!exit){
+            while (!exit) {
                 String input = in.readLine();
                 if (input != null) {
                     System.out.println(input);
@@ -30,9 +30,8 @@ public class Client {
                     out.flush();
                 }
             }
-            
         } finally {
-            socket.close();  // Ensure the socket is closed on exit
+            socket.close();
         }
     }
 }
